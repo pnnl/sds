edc4627a0e55080fe853e9805faebe0cb8422647
jessbade <jessicabade01@gmail.com>
2023-11-01 12:13:14 -0700

    Add testing for load csv, npz, tsv

--- tests/test_io.py
+++ tests/test_io.py
@@ -8,7 +8,7 @@ from tests import localfile
 
 @pytest.fixture
 def matrix():
-    return pd.read_csv(localfile("resources/toy-dataset.csv"))
+    return pd.read_csv(localfile("resources/toy-data.csv"))
 
 
 @pytest.fixture
@@ -51,6 +51,57 @@ def test_load_pickle(matrix):
 
     # Load
     testmatrix = sds.io.load_pickle(path)
+
+    # Check instance is correct type
+    assert isinstance(testmatrix, pd.DataFrame)
+
+    # Clean up
+    os.remove(path)
+
+
+def test_load_csv(matrix):
+    # Path to file
+    path = localfile("resources/toy-dataset.csv")
+
+    # Save to file
+    sds.save(path, matrix)
+
+    # Load
+    testmatrix = sds.io.load_csv(path)
+
+    # Check instance is correct type
+    assert isinstance(testmatrix, pd.DataFrame)
+
+    # Clean up
+    os.remove(path)
+
+
+def test_load_tsv(matrix):
+    # Path to file
+    path = localfile("resources/toy-dataset.tsv")
+
+    # Save to file
+    sds.save(path, matrix)
+
+    # Load
+    testmatrix = sds.io.load_tsv(path)
+
+    # Check instance is correct type
+    assert isinstance(testmatrix, pd.DataFrame)
+
+    # Clean up
+    os.remove(path)
+
+
+def test_load_numpy(matrix):
+    # Path to file
+    path = localfile("resources/toy-dataset.npz")
+
+    # Save to file
+    sds.save(path, matrix)
+
+    # Load
+    testmatrix = sds.io.load_numpy(path)
 
     # Check instance is correct type
     assert isinstance(testmatrix, pd.DataFrame)
